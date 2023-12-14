02344b1a6ce90567146ffa648fc25f3e93efe579
NGU516 <84605433+NGU516@users.noreply.github.com>
2023-12-14 13:09:40 +0900

    Update 3.cpp

--- Chapter09(복사생성자와 정적 멤버)/PROGRAMMING_EXERCISE/3.cpp
+++ Chapter09(복사생성자와 정적 멤버)/PROGRAMMING_EXERCISE/3.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
+#include <cstring>
 using namespace std;
 
-int main() {
-	cout << "hello" << endl;
+class MyClass{
+public:
+    MyClass(const char* str);
+    MyClass(const MyClass &other);
+    ~MyClass();
+private:
+    char* stored;
+};
+
+MyClass::MyClass(const char* str){
+    stored = new char[strlen(str)+1];
+    strcpy(stored, str);
+    cout << "일반 생성자" << endl;
+}
+
+MyClass::MyClass(const MyClass &other){
+    this->stored = new char[strlen(other.stored)+1];
+    strcpy(stored, other.stored);
+    cout << "복사 생성자" << endl;
+}
+
+MyClass::~MyClass(){
+    delete[] stored;
+}
+
+int main(){
+    MyClass obj("A반");
+    MyClass obj2(obj);
+
+    return 0;
 }
